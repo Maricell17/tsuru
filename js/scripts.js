1e44612f944894516790d21c874434c9a81b89c2
Maricell <49001490+Maricell17@users.noreply.github.com>
2021-10-25 22:50:58 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,145 @@
+
+
+
+//Desafío DOM
+
+function añadirCheesecake(){
+
+let a = document.getElementById("cheesecake");
+console.log("Cheesecake añadido al carrito");
+alert("Cheesecake añadido al carrito");
+
+   }
+
+
+function añadirPie(){
+
+    let a = document.getElementById("cheesecake");
+    console.log("Pie de limón añadido al carrito");
+    alert("Pie de limón añadido al carrito");
+    
+    }
+
+function añadirTarta(){
+
+    let a = document.getElementById("cheesecake");
+    console.log("Tarta de Frutas añadido al carrito");
+    alert("Tarta de Frutas añadido al carrito");
+        
+    }
+
+function añadirBrownie(){
+
+    let a = document.getElementById("cheesecake");
+    console.log("Brownie con nueces añadido al carrito");
+    alert("Brownie con nueces añadido al carrito");
+            
+    }
+
+function añadirBlondie(){
+
+    let a = document.getElementById("cheesecake");
+    console.log("Blondie añadido al carrito");
+    alert("Blondie añadido al carrito");
+                
+    }
+
+function añadirTrufas(){
+
+    let a = document.getElementById("cheesecake");
+    console.log("Trufas añadidas al carrito");
+    alert("Trufas añadidas al carrito");
+                    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//Acciones mediante prompt simulando proceso
+
+
+do{
+    let precioA = parseInt(prompt("Ingrese el primer precio"));
+    let precioB = parseInt(prompt("Ingrese el segundo precio"));
+    let suma = precioA + precioB;
+    alert( "La suma es: " + suma)
+    var opcion = prompt("Desear realizar otro calculo de precio: Si o No");
+}
+
+while(opcion != "No" && opcion != "no" && opcion != "NO");
+
+
+//Funciones
+
+function suma( productoA , productoB ){
+    console.log( productoA + productoB );
+}
+
+let productoA = parseInt (prompt("Ingrese precio del primer producto"));
+let productoB = parseInt (prompt("Ingrese precio del segundo producto"));
+
+suma ( productoA , productoB );
+
+
+
+
+//Arrays y productos
+
+
+class producto{
+    
+    constructor( codigo , nombre , precio ){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+
+    PrecioFinal(){
+
+        console.log( this.precio + (this.precio * 0.19));
+    }
+
+}
+
+let producto1 = new producto( 001 , "Cheesecake" , 2500 );
+let producto2 = new producto( 002 , "Pie de limon" , 2300 );
+let producto3 = new producto( 003 , "Tarta de frutas" , 1800 );
+let producto4 = new producto( 004 , "Brownie" , 1500 );
+
+const productos = [ ];
+
+productos.push(new producto(001, "Cheesecake", 2500 ));
+productos.push(new producto(002, "Pie de limon", 2300 ));
+productos.push(new producto(003, "Tarta de frutas", 1800 ));
+productos.push(new producto(004, "Brownie", 1500 ));
+
+for (const producto of productos)
+
+producto.PrecioFinal();
+
+productos.push("Trufas");
+
+console.log(productos);
+*/
+
